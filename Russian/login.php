9cf433b9f0c87a6ed213ca44935d07f0d7d4356f
adelf <adelf@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-11-14 17:48:17 +0000

    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@7 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/login.php
+++ Russian/login.php
@@ -1,32 +1,32 @@
 <?php
 
-// Language definitions used in delete.php
+// Language definitions used in login.php
 $lang_login = array(
 
 // Miscellaneous
-'Login'								=>	'Login',
-'Login info'						=>	'Login to %s',
-'Username'							=>	'Username',
-'Password'							=>	'Password',
-'Remember me'						=>	'Remember me',
-'E-mail address'					=>	'E-mail address',
-'E-mail address help'				=>	'Enter the e-mail address set in your profile.',
-'Login errors'						=>	'<strong>Warning!</strong> The following errors must be corrected before you can login:',
-'Wrong user/pass'					=>	'Incorrect username and/or password.',
-'Login redirect'					=>	'Logged in successfully. Redirecting …',
-'Logout redirect'					=>	'Logged out. Redirecting …',
-'No e-mail match'					=>	'There is no user registered with the e-mail address %s.',
-'Invalid e-mail'					=>	'The e-mail address you entered is invalid.',
-'Login options'						=>	'Do you need to %1$s or obtain a %2$s before you login?',
-'Obtain pass'						=>	'new password',
-'register'							=>	'register',
-'Submit password request'			=>	'Submit request',
-'Email flood'						=>	'This account has already requested a new password in the past %s seconds. Please wait a while before requesting a new password again.',
-'New password request'				=>	'New password request',
-'New password errors'				=>	'<strong>Warning!</strong> The following errors must be corrected before a new password can be sent:',
-'New password info'					=>	'<strong>Important!</strong> An e-mail will be sent to the specified address with instructions on how to change your password.',
-'New password cancel redirect'		=>	'New password request cancelled. Redirecting …',
-'Forget mail'						=>	'An e-mail has been sent to the specified address with instructions on how to change your password. If it does not arrive you can contact the forum administrator at %s.',
-'Persistent login'					=>	'Log me in automatically each time I visit.'
+'Login' 						 => 'Войти',    //Login
+'Login info'					 => 'Вход на форум %s.',    //Login to %s
+'Username'  					 => 'Имя пользователя',    //Username
+'Password'  					 => 'Пароль',    //Password
+'Remember me'   				 => 'Запомнить меня',    //Remember me
+'E-mail address'				 => 'E-mail',    //E-mail address
+'E-mail address help'   		 => 'Введите свой e-mail адрес.',    //Enter the e-mail address set in your profile.
+'Login errors'  				 => '<strong>Внимание!</strong> При входе на форум произошли следующие ошибки:',    //<strong>Warning!</strong> The following errors must be corrected before you can login:
+'Wrong user/pass'   			 => 'Неправильное имя и/или пароль.',    //Incorrect username and/or password.
+'Login redirect'				 => 'Успешный вход. Переадресация…',    //Logged in successfully. Redirecting …
+'Logout redirect'   			 => 'Вы вышли. Переадресация…',    //Logged out. Redirecting …
+'No e-mail match'   			 => 'Зарегистрированного пользователя с e-mail адресом %s не существует',    //There is no user registered with the e-mail address %s.
+'Invalid e-mail'				 => 'E-mail адрес, который вы ввели, неправилен.',    //The e-mail address you entered is invalid.
+'Login options' 				 => 'Возможно вам стоит %1$s или %2$s перед тем как входить на форум',    //Do you need to %1$s or obtain a %2$s before you login?
+'Obtain pass'   				 => 'напомнить пароль',    //new password
+'register'  					 => 'зарегистрироваться',    //register
+'Submit password request'   	 => 'Отправить запрос',    //Submit request
+'Email flood'   				 => 'Этот аккаунт уже запросил новый пароль около %s секунд назад. Пожалуйста, подождите немного, прежде чем снова запрашивать новый пароль.',    //This account has already requested a new password in the past %s seconds. Please wait a while before requesting a new password again.
+'New password request'  		 => 'Запрос на новый пароль',    //New password request
+'New password errors'   		 => '<strong>Внимание!</strong> Исправьте следующие ошибки:',    //<strong>Warning!</strong> The following errors must be corrected before a new password can be sent:
+'New password info' 			 => '<strong>Важно!</strong> По введенному вами e-mail адресу будет выслано письмо с инструкциями по смене пароля.',    //<strong>Important!</strong> An e-mail will be sent to the specified address with instructions on how to change your password.
+'New password cancel redirect'   => 'Запрос нового пароля отменен. Переадресация…',    //New password request cancelled. Redirecting …
+'Forget mail'   				 => 'Сообщение выслано на указанный адрес вместе с инструкцией по изменению пароля. В случае каких-либо проблем Вы можете обратиться к администратору',    //An e-mail has been sent to the specified address with instructions on how to change your password. If it does not arrive you can contact the forum administrator at %s.
+'Persistent login'  			 => 'Входить автоматически в следующий раз',    //Log me in automatically each time I visit.
 
 );
--- Russian/misc.php
+++ Russian/misc.php
@@ -3,97 +3,97 @@
 // Language definitions used in various scripts
 $lang_misc = array(
 
-'Mark read redirect'		=>	'All topics and forums have been marked as read. Redirecting …',
-'Mark forum read redirect'	=>	'All topics in the specified forum have been marked as read. Redirecting …',
+'Mark read redirect'		 => 'Все темы и форумы были отмечены как прочтённые. Переадресация…',    //All topics and forums have been marked as read. Redirecting …
+'Mark forum read redirect'   => 'Все темы в этом форуме были отмечены как прочтённые. Переадресация…',    //All topics in the specified forum have been marked as read. Redirecting …
 
 // Send e-mail
-'Form e-mail disabled'		=>	'The user you are trying to send an e-mail to has disabled form e-mail.',
-'Form e-mail errors'		=>	'<strong>Warning!</strong> The following errors must be corrected before your message can be sent:',
-'No e-mail subject'			=>	'You must enter a subject.',
-'No e-mail message'			=>	'You must enter a message.',
-'Too long e-mail message'	=>	'Your message length is %s bytes. This exceeds the %s bytes limit.',
-'Email flood'				=>	'At least %s seconds have to pass between sent emails. Please wait a while and try sending again.',
-'E-mail sent redirect'		=>	'E-mail sent. Redirecting …',
-'E-mail subject'			=>	'Subject',
-'E-mail message'			=>	'Message',
-'E-mail disclosure note'	=>	'<strong>Important!</strong> When you send an e-mail using this form your e-mail address will be disclosed to the recipient.',
-'Write e-mail'				=>	'Compose e-mail',
-'Send forum e-mail'			=>	'Send e-mail to %s via the forum',
+'Form e-mail disabled'  	 => 'Пользователь, которому Вы хотите послать сообщение, отказался от получения сообщений по e-mail.',    //The user you are trying to send an e-mail to has disabled form e-mail.
+'Form e-mail errors'		 => '<strong>Внимание!</strong> Исправьте следующие ошибки для отправки вашего сообщения:',    //<strong>Warning!</strong> The following errors must be corrected before your message can be sent:
+'No e-mail subject' 		 => 'Вы должы ввести тему сообщения.',    //You must enter a subject.
+'No e-mail message' 		 => 'Вы должны ввести текст сообщения.',    //You must enter a message.
+'Too long e-mail message'    => 'Длина вашего сообщения %s байт. Это превышает лимит форума в %s байтов.',    //Your message length is %s bytes. This exceeds the %s bytes limit.
+'Email flood'   			 => 'Должно пройти не менее %s секунд между отправками сообщений. Пожалуйста подождите и попробуйте еще раз.',    //At least %s seconds have to pass between sent emails. Please wait a while and try sending again.
+'E-mail sent redirect'  	 => 'Сообщение послано. Переадресация…',    //E-mail sent. Redirecting …
+'E-mail subject'			 => 'Заголовок',    //Subject
+'E-mail message'			 => 'Сообщение',    //Message
+'E-mail disclosure note'	 => '<strong>Важно!</strong> Имейте в виду, что при отправлении сообщения через эту форму получатель узнает ваш e-mail адрес.',    //<strong>Important!</strong> When you send an e-mail using this form your e-mail address will be disclosed to the recipient.
+'Write e-mail'  			 => 'Напишите сообщение и нажмите отправить',    //Compose e-mail
+'Send forum e-mail' 		 => 'Послать сообщение пользователю %s используя форум',    //Send e-mail to %s via the forum
 
 // Report
-'No reason'					=>	'You must enter a reason.',
-'Report flood'				=>	'At least %s seconds have to pass between reports. Please wait a while and try sending again.',
-'Report redirect'			=>	'Post reported. Redirecting …',
-'Report post'				=>	'Report post',
-'Reason'					=>	'Reason',
-'Reason help'				=>	'Enter a short reason why you are reporting this post.',
+'No reason' 				 => 'Вы должны описать что произошло.',    //You must enter a reason.
+'Report flood'  			 => 'Должно пройти не менее %s секунд между сообщениями модераторам. Пожалуйста подождите и попробуйте еще раз.',    //At least %s seconds have to pass between reports. Please wait a while and try sending again.
+'Report redirect'   		 => 'Сообщение отправлено. Переадресация…',    //Post reported. Redirecting …
+'Report post'   			 => 'Сообщить модератору',    //Report post
+'Reason'					 => 'Причина сообщения',    //Reason
+'Reason help'   			 => 'Укажите кратко причину сообщения',    //Enter a short reason why you are reporting this post.
 
 // Subscriptions
-'Already subscribed'		=>	'You are already subscribed to this topic.',
-'Subscribe redirect'		=>	'Your subscription has been added. Redirecting …',
-'Not subscribed'			=>	'You are not subscribed to this topic.',
-'Unsubscribe redirect'		=>	'Your subscription has been removed. Redirecting …',
+'Already subscribed'		 => 'Вы уже подписаны на эту тему.',    //You are already subscribed to this topic.
+'Subscribe redirect'		 => 'Подписка добавлена. Переадресация…',    //Your subscription has been added. Redirecting …
+'Not subscribed'			 => 'Вы не подписаны на эту тему.',    //You are not subscribed to this topic.
+'Unsubscribe redirect'  	 => 'Вы отписались. Переадресация…',    //Your subscription has been removed. Redirecting …
 
 // General forum and topic moderation
-'Moderate forum'			=>	'Moderate forum',
-'Select'					=>	'Select',	// the header of a column of checkboxes
-'Move'						=>	'Move',
-'Merge'						=>	'Merge',
-'Open'						=>  'Open',
-'Close'						=>  'Close',
-'Select all'				=>	'Select all',
+'Moderate forum'			 => 'Модерировать форум',    //Moderate forum
+'Select'					 => 'Выбрать',    //Select
+'Move'  					 => 'Переместить',    //Move
+'Merge' 					 => 'Соединить',    //Merge
+'Open'  					 => 'Открыть',    //Open
+'Close' 					 => 'Закрыть',    //Close
+'Select all'				 => 'Выбрать все',    //Select all
 
 
 // Hostname lookup
-'Hostname lookup'			=>	'The IP address is: %1$s<br />The host name is: %2$s<br /><br />%3$s',
-'Show more users'			=>	'Show more users for this IP',
+'Hostname lookup'   		 => 'IP адрес: %1$s<br />Имя хоста: %2$s<br /><br />%3$s',    //The IP address is: %1$s<br />The host name is: %2$s<br /><br />%3$s
+'Show more users'   		 => 'Показать пользователей использовавших этот IP',    //Show more users for this IP
 
 // Moderate forum
-'Moderate forum head'		=>	'Moderate : %s',
-'Topics'					=>	'Topics',
-'Move topic'				=>	'Move topic',
-'Move topics'				=>	'Move topics',
-'Merge topics'				=>	'Merge topics',
-'Delete topics'				=>	'Delete topics',
-'Delete topic'				=>	'Delete topic',
-'To new forum'				=>	'to a new forum',
-'Move to'					=>	'Destination forum',
-'Redirect topic'			=>	'Redirect setting',
-'Nowhere to move'			=>	'There are no forums into which you can move topics.',
-'Leave redirect'			=>	'Leave a redirect topic in the forum from which the topic was moved.',
-'Leave redirects'			=>	'Leave redirect topics in the forum from which topics were moved.',
-'Leave merge redirects'		=>	'Leave redirect topics for those topics which were merged.',
-'Move topic redirect'		=>	'Topic moved. Redirecting …',
-'Move topics redirect'		=>	'Topics moved. Redirecting …',
-'Merge topics redirect'		=>	'Topics merged. Redirecting …',
-'Delete topic comply'		=>	'Are you sure you want to delete the selected topic?',
-'Delete topics comply'		=>	'Are you sure you want to delete all the selected topics?',
-'Delete topic redirect'		=>	'Topic deleted. Redirecting …',
-'Delete topics redirect'	=>	'Topics deleted. Redirecting …',
-'Open topic redirect'		=>	'Topic opened. Redirecting …',
-'Open topics redirect'		=>	'Topics opened. Redirecting …',
-'Close topic redirect'		=>	'Topic closed. Redirecting …',
-'Close topics redirect'		=>	'Topics closed. Redirecting …',
-'No topics selected'		=>	'You must select at least one topic.',
-'Stick topic redirect'		=>	'Topic is now sticky. Redirecting …',
-'Unstick topic redirect'	=>	'Topic no longer sticky. Redirecting …',
-'Merge error'				=>	'You should select more than 1 topic to merge.',
+'Moderate forum head'   	 => 'Модерация : %s',    //Moderate : %s
+'Topics'					 => 'Темы',    //Topics
+'Move topic'				 => 'Переместить тему',    //Move topic
+'Move topics'   			 => 'Переместить темы',    //Move topics
+'Merge topics'  			 => 'Соединить темы',    //Merge topics
+'Delete topics' 			 => 'Удалить темы',    //Delete topics
+'Delete topic'  			 => 'Удалить тему',    //Delete topic
+'To new forum'  			 => 'в новый форум',    //to a new forum
+'Move to'   				 => 'Переместить в',    //Destination forum
+'Redirect topic'			 => 'Переадресация',    //Redirect setting
+'Nowhere to move'   		 => 'Нет ни одного форума, в который вы могли бы переносить темы.',    //There are no forums into which you can move topics.
+'Leave redirect'			 => 'Оставить ссылку на темы/тему в этом форуме',    //Leave a redirect topic in the forum from which the topic was moved.
+'Leave redirects'   		 => 'Оставить ссылку на перемещаемые темы/тему в этом форуме.',    //Leave redirect topics in the forum from which topics were moved.
+'Leave merge redirects' 	 => 'Оставить ссылку на соединяемые темы/тему.',    //Leave redirect topics for those topics which were merged.
+'Move topic redirect'   	 => 'Тема перемещена. Переадресация…',    //Topic moved. Redirecting …
+'Move topics redirect'  	 => 'Темы перемещены. Переадресация…',    //Topics moved. Redirecting …
+'Merge topics redirect' 	 => 'Темы соединены. Переадресация…',    //Topics merged. Redirecting …
+'Delete topic comply'   	 => 'Вы уверены, что хотите удалить выделенную тему?',    //Are you sure you want to delete the selected topic?
+'Delete topics comply'  	 => 'Вы уверены, что хотите удалить выделенные темы?',    //Are you sure you want to delete all the selected topics?
+'Delete topic redirect' 	 => 'Тема удалена. Переадресация…',    //Topic deleted. Redirecting …
+'Delete topics redirect'	 => 'Темы удалены. Переадресация…',    //Topics deleted. Redirecting …
+'Open topic redirect'   	 => 'Тема открыта. Переадресация…',    //Topic opened. Redirecting …
+'Open topics redirect'  	 => 'Темы открыты. Переадресация…',    //Topics opened. Redirecting …
+'Close topic redirect'  	 => 'Тема закрыта. Переадресация…',    //Topic closed. Redirecting …
+'Close topics redirect' 	 => 'Темы закрыты. Переадресация…',    //Topics closed. Redirecting …
+'No topics selected'		 => 'Вы должны выбрать, по крайней мере, одну тему, для выполнения этого действия.',    //You must select at least one topic.
+'Stick topic redirect'  	 => 'Тема выделена. Переадресация…',    //Topic is now sticky. Redirecting …
+'Unstick topic redirect'	 => 'Выделение с темы снято. Переадресация…',    //Topic no longer sticky. Redirecting …
+'Merge error'   			 => 'Вы должны выбрать больше одной темы для соединения.',    //You should select more than 1 topic to merge.
 
 // Moderate topic
-'Posts'						=>	'Posts',
-'Delete posts'				=>	'Delete selected posts',
-'Split posts'				=>	'Split selected posts',
-'Delete whole topic'		=>	'Delete whole topic',
-'Moderate topic head'		=>	'Moderate topic : %s',
-'New subject'				=>	'New topic subject:',
-'Select post'				=>	'Select post', // Label for checkbox
-'Confirm post delete'		=>	'Confirm deletion of all selected posts',
-'Confirm topic delete'		=>	'Confirm deletion of all selected topics',
-'Confirm topic split'		=>	'Confirm splitting of all selected posts into a new topic',
-'Confirm topic merge'		=>	'Confirm merging of all selected topics into one topic',
-'Confirm post split'		=>	'Confirm splitting of all selected posts',
-'Delete posts redirect'		=>	'Posts deleted. Redirecting …',
-'Split posts redirect'		=>	'Posts split into a new topic. Redirecting …',
-'No posts selected'			=>	'You must select at least one post.'
+'Posts' 					 => 'Сообщения',    //Posts
+'Delete posts'  			 => 'Удалить сообщения',    //Delete selected posts
+'Split posts'   			 => 'Переместить выделенные сообщения',    //Split selected posts
+'Delete whole topic'		 => 'Удалить всю тему',    //Delete whole topic
+'Moderate topic head'   	 => 'Модерация темы : %s',    //Moderate topic : %s
+'New subject'   			 => 'Новый заголовок темы:',    //New topic subject:
+'Select post'   			 => 'Выделить сообщение',    //Select post
+'Confirm post delete'   	 => 'Подтвердите удаление всех выбранных сообщений',    //Confirm deletion of all selected posts
+'Confirm topic delete'  	 => 'Подтвердите удаление всех выбранных тем',    //Confirm deletion of all selected topics
+'Confirm topic split'   	 => 'Подвердите перемещение всех выделенных сообщений в новую тему',    //Confirm splitting of all selected posts into a new topic
+'Confirm topic merge'   	 => 'Подтверждение соединения тем в одну',    //Confirm merging of all selected topics into one topic
+'Confirm post split'		 => 'Подтверждение перемещения сообщений',    //Confirm splitting of all selected posts
+'Delete posts redirect' 	 => 'Сообщения удалены. Переадресация…',    //Posts deleted. Redirecting …
+'Split posts redirect'  	 => 'Собщения перемещены в новую тему. Переадресация…',    //Posts split into a new topic. Redirecting …
+'No posts selected' 		 => 'Вы должны выбрать, по крайней мере, одно сообщение, для выполнения этого действия.',    //You must select at least one post.
 
 );
