ff782cfbb1e1491267c268b17513d79415df274b
Freeman <Freeman@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-11-18 12:48:13 +0000

    Начальный перевод common.php, см. также // TODO
    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@76 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/common.php
+++ Russian/common.php
@@ -12,155 +12,155 @@ $lang_common = array(
 'lang_thousands_sep'	=>	',',
 
 // Notices
-'Bad request'				=>	'Bad request. The link you followed is incorrect or outdated.',
-'No view'					=>	'You do not have permission to view these forums.',
-'No permission'				=>	'You do not have permission to access this page.',
-'CSRF token mismatch'		=>	'Unable to confirm security token. A likely cause for this is that some time passed between when you first entered the page and when you submitted a form or clicked a link. If that is the case and you would like to continue with your action, please click the Confirm button. Otherwise, you should click the Cancel button to return to where you were.',
-'No cookie'					=>	'You appear to have logged in successfully, however a cookie has not been set. Please check your settings and if applicable, enable cookies for this website.',
+'Bad request'				=>	'Неверный запрос. Ссылка, по которой вы пришли, неверна или устарела.',
+'No view'					=>	'У вас нет прав просмотра данных разделов.',
+'No permission'				=>	'У вас нет прав доступа к данной странице.',
+'CSRF token mismatch'		=>	'Невозможно подтвердить маркер доступа. Возможно, прошло некоторое время после того, как вы впервые вошли на страницу и последующей отправкой формы или щелчком по ссылке. Если это так, и вы хотите выполнить своё действие до конца, пожалуйста, нажмите кнопку "Продолжить". Иначе нужно нажать кнопку "Отмена" для возврата на исходную страницу.',
+'No cookie'					=>	'Вы успешно вошли под своей учётной записью, но cookie не установлен. Пожалуйста, проверьте свои настройки и, если возможно, разрешите использование cookie для данного сайта.',
 
 
 // Miscellaneous
-'Forum index'				=>	'Forum index',
-'Submit'					=>	'Submit',	// "name" of submit buttons
-'Cancel'					=>	'Cancel', // "name" of cancel buttons
-'Preview'					=>	'Preview',	// submit button to preview message
-'Delete'					=>	'Delete',
-'Split'						=>	'Split',
-'Ban message'				=>	'You are banned from this forum.',
-'Ban message 2'				=>	'The ban expires at the end of %s.',
-'Ban message 3'				=>	'The administrator or moderator that banned you left the following message:',
-'Ban message 4'				=>	'Please direct any inquiries to the forum administrator at %s.',
-'Never'						=>	'Never',
-'Today'						=>	'Today',
-'Yesterday'					=>	'Yesterday',
-'Forum message'				=>	'Forum message',
-'Maintenance warning'		=>	'<strong>WARNING! %s Enabled.</strong> DO NOT LOGOUT as you will be unable to login again.',
-'Maintenance mode'			=>	'Maintenance Mode',
-'Redirecting'				=>	'Redirecting',
-'Forwarding info'			=>	'You should automatically be forwarded to a new page in %s %s.',
-'second'					=>	'second',	// singular
-'seconds'					=>	'seconds',	// plural
-'Click redirect'			=>	'Click here if you do not want to wait any longer (or if your browser does not automatically forward you)',
-'Invalid e-mail'			=>	'The e-mail address you entered is invalid.',
-'New posts'					=>	'New posts',	// the link that leads to the first new post
-'New posts title'			=>	'Find topics containing posts made since your last visit.',	// the popup text for new posts links
-'Active topics'				=>	'Active topics',
-'Active topics title'		=>	'Find topics which contain recent posts.',
-'Unanswered topics'			=>	'Unanswered topics',
-'Unanswered topics title'	=>	'Find topics which have not been replied to.',
-'Username'					=>	'Username',
-'Registered'				=>	'Registered',
-'Write message'				=>	'Write message:',
-'Forum'						=>	'Forum',
-'Posts'						=>	'Posts',
-'Pages'						=>	'Pages',
-'Page'						=>	'Page',
+'Forum index'				=>	'Оглавление раздела',
+'Submit'					=>	'Отправить',	// "name" of submit buttons
+'Cancel'					=>	'Отмена', // "name" of cancel buttons
+'Preview'					=>	'Посмотреть',	// submit button to preview message
+'Delete'					=>	'Удалить',
+'Split'						=>	'Разделить',
+'Ban message'				=>	'Вы заблокированы в данном разделе.',
+'Ban message 2'				=>	'Блокировка истекает %s.',
+'Ban message 3'				=>	'Администратор или модератор заблокировал вас и оставил следующее сообщение:',
+'Ban message 4'				=>	'За дальнейшими разъяснениями обращайтесь к администратору раздела: %s.',
+'Never'						=>	'Никогда',
+'Today'						=>	'Сегодня',
+'Yesterday'					=>	'Вчера',
+'Forum message'				=>	'Сообщение в разделе',
+'Maintenance warning'		=>	'<strong>ПРЕДУПРЕЖДЕНИЕ! Включён %s.</strong> НЕ ВЫХОДИТЕ ИЗ-ПОД УЧЁТНОЙ ЗАПИСИ - вы не сможете войти обратно.',
+'Maintenance mode'			=>	'режим профилактики',
+'Redirecting'				=>	'Перенаправление',
+'Forwarding info'			=>	'Вы будете автоматически перенаправлены на новую страницу через %s %s.',
+'second'					=>	'секунда',	// singular
+'seconds'					=>	'секунд',	// plural
+'Click redirect'			=>	'Нажмите, если не хотите ждать (или если ваш браузер не перенаправляет автоматически)',
+'Invalid e-mail'			=>	'Введённый вами адрес e-mail неверен.',
+'New posts'					=>	'Новые сообщения',	// the link that leads to the first new post
+'New posts title'			=>	'Найти темы с сообщениями после вашего последнего посещения.',	// the popup text for new posts links
+'Active topics'				=>	'Активные темы',
+'Active topics title'		=>	'Найти темы с недавними сообщениями.',
+'Unanswered topics'			=>	'Неотвеченные темы',
+'Unanswered topics title'	=>	'Найти темы, на которые не было ответов.',
+'Username'					=>	'Имя пользователя',
+'Registered'				=>	'Зарегистрирован',
+'Write message'				=>	'Напишите сообщение:',
+'Forum'						=>	'Раздел',
+'Posts'						=>	'Сообщения',
+'Pages'						=>	'Страницы',
+'Page'						=>	'Страница',
 'BBCode'					=>	'BBCode',	// You probably shouldn't change this
-'Smilies'					=>	'Smilies',
-'Images'					=>	'Images',
-'You may use'				=>	'You may use: %s',
-'and'						=>	'and',
-'Image link'				=>	'image',	// This is displayed (i.e. <image>) instead of images when "Show images" is disabled in the profile
-'wrote'						=>	'wrote',	// For [quote]'s (e.g., User wrote:)
-'Code'						=>	'Code',		// For [code]'s
-'Forum mailer'				=>	'%s Mailer',	// As in "MyForums Mailer" in the signature of outgoing e-mails
-'Write message legend'		=>	'Compose your post',
-'Required information'		=>	'Required information',
+'Smilies'					=>	'Смайлики',
+'Images'					=>	'Изображения',
+'You may use'				=>	'Вы можете использовать: %s',
+'and'						=>	'и',
+'Image link'				=>	'изображение',	// This is displayed (i.e. <image>) instead of images when "Show images" is disabled in the profile
+'wrote'						=>	'писал(а)',	// For [quote]'s (e.g., User wrote:)
+'Code'						=>	'Код',		// For [code]'s
+'Forum mailer'				=>	'Почтовый робот форума "%s"',	// As in "MyForums Mailer" in the signature of outgoing e-mails
+'Write message legend'		=>	'Составьте сообщение',
+'Required information'		=>	'Обязательно к заполнению',
 'Reqmark'					=>	'*',
-'Required'					=>	'(Required)',
-'Required warn'				=>	'All fields labelled %s must be completed before the form is submitted.',
+'Required'					=>	'(Обязательно)',
+'Required warn'				=>	'Все поля, отмеченные знаком "%s", должны быть заполнены перед отправкой формы.',
 'Crumb separator'			=>	' »&#160;', // The character or text that separates links in breadcrumbs
-'Title separator'			=>	' - ',
+'Title separator'			=>	' &mdash; ',
 'Page separator'			=>	'&#160;', //The character or text that separates page numbers
 'Spacer'					=>	'…', // Ellipsis for paginate
 'Paging separator'			=>	' ', //The character or text that separates page numbers for page navigation generally
-'Previous'					=>	'Previous',
-'Next'						=>	'Next',
-'Cancel redirect'			=>	'Operation cancelled. Redirecting …',
-'No confirm redirect'		=>	'No confirmation provided. Operation cancelled. Redirecting …',
-'Please confirm'			=>	'Please confirm:',
-'Help page'					=>	'Help with: %s',
+'Previous'					=>	'Назад',
+'Next'						=>	'Далее',
+'Cancel redirect'			=>	'Операция отменена. Перенаправление…',
+'No confirm redirect'		=>	'Подтверждение не получено. Операция отменена. Перенаправление…',
+'Please confirm'			=>	'Пожалуйста, подтвердите:',
+'Help page'					=>	'Справка по: %s',
 'Re'						=>	'Re:',
-'Page info'					=>	'(Page %1$s of %2$s)',
+'Page info'					=>	'(Страница %1$s из %2$s)',
 'Item info single'			=>	'%s [ %s ]',
-'Item info plural'			=>	'%s [ %s to %s of %s ]', // e.g. Topics [ 10 to 20 of 30 ]
+'Item info plural'			=>	'%s [ с %s по %s из %s ]', // e.g. Topics [ 10 to 20 of 30 ]
 'Info separator'			=>	' ', // e.g. 1 Page | 10 Topics
-'Powered by'				=>	'Powered by <strong>%s</strong>',
-'Maintenance'				=>	'Maintenance',
+'Powered by'				=>	'На основе <strong>%s</strong>',
+'Maintenance'				=>	'Профилактика',
 
 // CSRF confirmation form
-'Confirm'					=>	'Confirm',	// Button
-'Confirm action'			=>	'Confirm action',
-'Confirm action head'		=>	'Please confirm or cancel your last action',
+'Confirm'					=>	'Подтвердить',	// Button
+'Confirm action'			=>	'Подтверждение',
+'Confirm action head'		=>	'Пожалуйста, подтвердите или отмените своё действие',
 
 // Title
-'Title'						=>	'Title',
-'Member'					=>	'Member',	// Default title
-'Moderator'					=>	'Moderator',
-'Administrator'				=>	'Administrator',
-'Banned'					=>	'Banned',
-'Guest'						=>	'Guest',
+'Title'						=>	'Заголовок',
+'Member'					=>	'Участник',	// Default title
+'Moderator'					=>	'Модератор',
+'Administrator'				=>	'Администратор',
+'Banned'					=>	'Заблокирован',
+'Guest'						=>	'Гость',
 
 // Stuff for include/parser.php
-'BBCode error 1'			=>	'[/%1$s] was found without a matching [%1$s]',
-'BBCode error 2'			=>	'[%s] tag is empty',
-'BBCode error 3'			=>	'[%1$s] was opened within [%2$s], this is not allowed',
-'BBCode error 4'			=>	'[%s] was opened within itself, this is not allowed',
-'BBCode error 5'			=>	'[%1$s] was found without a matching [/%1$s]',
-'BBCode error 6'			=>	'[%s] tag had an empty attribute section',
-'BBCode nested list'		=>	'[list] tags cannot be nested',
-'BBCode code problem'		=>	'There is a problem with your [code] tags',
+'BBCode error 1'			=>	'Найден тег [/%1$s] без соответствующего [%1$s]',
+'BBCode error 2'			=>	'Тег [%s] пуст',
+'BBCode error 3'			=>	'Использование тега [%1$s] внутри [%2$s] не разрешается',
+'BBCode error 4'			=>	'Вложение тега [%s] в самого себя не разрешается',
+'BBCode error 5'			=>	'Найден тег [%1$s] без соответствующего [/%1$s]',
+'BBCode error 6'			=>	'Тег [%s] имеет пустой раздел атрибутов',
+'BBCode nested list'		=>	'Теги [list] не нужны',
+'BBCode code problem'		=>	'Проблема с тегами [code]',
 
 // Stuff for the navigator (top of every page)
-'Index'						=>	'Index',
-'User list'					=>	'User list',
-'Rules'						=>  'Rules',
-'Search'					=>  'Search',
-'Register'					=>  'Register',
-'register'					=>	'register',
-'Login'						=>  'Login',
-'login'						=>	'login',
-'Not logged in'				=>  'You are not logged in.',
-'Profile'					=>	'Profile',
-'Logout'					=>	'Logout',
-'Logged in as'				=>	'Logged in as %s.',
-'Admin'						=>	'Administration',
-'Last visit'				=>	'Last visit %s',
-'Mark all as read'			=>	'Mark all topics as read',
-'Login nag'					=>	'Please login or register.',
-'New reports'				=>	'New reports',
+'Index'						=>	'Оглавление',
+'User list'					=>	'Список пользователей',
+'Rules'						=>  'Правила',
+'Search'					=>  'Поиск',
+'Register'					=>  'Регистрация',
+'register'					=>	'регистрация',
+'Login'						=>  'Вход',
+'login'						=>	'вход',
+'Not logged in'				=>  'Вы не вошли.',
+'Profile'					=>	'Профиль',
+'Logout'					=>	'Выйти',
+'Logged in as'				=>	'Вы вошли как %s.',
+'Admin'						=>	'Администрирование',
+'Last visit'				=>	'Последнее посещение %s',
+'Mark all as read'			=>	'Отметить все темы прочитанными',
+'Login nag'					=>	'Пожалуйста, войдите или зарегистрируйтесь.',
+'New reports'				=>	'Новые обращения',
 
 // Alerts
-'New alerts'				=>	'New Alerts',
-'Maintenance alert'			=>	'<strong>WARNING! Maintenance mode enabled.</strong> This board is currently in maintenance mode. <em>DO NOT</em> logout, if you do you will not be able to login again.',
-'Updates'					=>	'PunBB updates',
-'Updates failed'			=>	'The latest attempt at checking for updates against the punbb.informer.com updates service failed. This probably just means that the service is temporarily overloaded or out of order. However, if this alert does not disappear within a day or two, you should disable the automatic check for updates and check for updates manually in the future.',
-'Updates version n hf'		=>	'A newer version of PunBB, version %s, is available for download at <a href="http://punbb.informer.com/">punbb.informer.com</a>. Furthermore, one or more hotfix extensions are available for install on the Extensions tab of the admin interface.',
-'Updates version'			=>	'A newer version of PunBB, version %s, is available for download at <a href="http://punbb.informer.com/">punbb.informer.com</a>.',
-'Updates hf'				=>	'One or more hotfix extensions are available for install on the Extensions tab of the admin interface.',
-'Database mismatch'			=>	'Database version mismatch',
-'Database mismatch alert'	=>	'Your PunBB database is meant to be used in conjunction with a newer version of the PunBB code. This mismatch can lead to your forum not working properly. It is suggested that you upgrade your forum to the newest version of PunBB.',
+'New alerts'				=>	'Новые предупреждения',
+'Maintenance alert'			=>	'<strong>ПРЕДУПРЕЖДЕНИЕ! Включён режим профилактики.</strong> Форум находится в режиме профилактики. <em>НЕ</em> выходите из-под своей учётной записи - вы не сможете войти обратно.',
+'Updates'					=>	'Обновления PunBB',
+'Updates failed'			=>	'Последняя попытка проверки обновлений на punbb.informer.com завершилась неудачно. Это может означать, что сервис обновлений перегружен или недоступен. Если данное предупреждение не исчезнет в течение одного-двух дней, вы можете отключить автоматическую проверку и проверять наличие обновлений вручную.',
+'Updates version n hf'		=>	'Новая версия PunBB - %s доступна для загрузки на <a href="http://punbb.informer.com/">punbb.informer.com</a>. Кроме этого, одно или несколько исправлений расширений доступно для установки на вкладке "Расширения" административного интерфейса.',
+'Updates version'			=>	'Новая версия PunBB - %s доступна для загрузки на <a href="http://punbb.informer.com/">punbb.informer.com</a>.',
+'Updates hf'				=>	'Одно или несколько исправлений расширений доступно для установки на вкладке "Расширения" административного интерфейса.',
+'Database mismatch'			=>	'Несоответствие версии базы данных',
+'Database mismatch alert'	=>	'База данных PunBB будет использована в связке с новой версией кода PunBB. Несоответствие может привести к неработоспособности форума. Рекомендуется обновить форум до самой новой версии PunBB.',
 
 // Stuff for Jump Menu
-'Go'						=>	'Go',		// submit button in forum jump
-'Jump to'					=>	'Jump to forum:',
+'Go'						=>	'Перейти',		// submit button in forum jump
+'Jump to'					=>	'Перейти в раздел:',
 
 // For extern.php RSS feed
 'ATOM Feed'					=>	'Atom',
 'RSS Feed'					=>	'RSS',
-'RSS description'			=>	'The most recent topics at %s.',
-'RSS description topic'		=>	'The most recent posts in %s.',
+'RSS description'			=>	'Недавние темы %s.',
+'RSS description topic'		=>	'Недавние сообщения в %s.',
 'RSS reply'					=>	'Re: ',	// The topic subject will be appended to this string (to signify a reply)
 
 // Accessibility
-'Skip to content'					=>	'Skip to forum content',
+'Skip to content'					=>	'Skip to forum content', // TODO
 
 // Debug information
-'Querytime'						=>	'Generated in %1$s seconds, %2$s queries executed',
-'Debug table'						=>	'Debug information',
-'Debug summary'						=>	'Database query performance information',
-'Query times'						=>	'Time (s)',
-'Query'							=>	'Query',
-'Total query time'					=>	'Total query time',
+'Querytime'						=>	'Сгенерировано за %1$s секунд(ы), выполнено %2$s запросов',
+'Debug table'						=>	'Отладочная информация',
+'Debug summary'						=>	'Информация о производительности базы данных',
+'Query times'						=>	'Время (с)',
+'Query'							=>	'Запрос',
+'Total query time'					=>	'Общее время запроса',
 
 );
