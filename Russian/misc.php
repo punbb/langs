d2a815bfa32c44721928d531df7ead47bcc74747
TuxPirate <TuxPirate@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-11-20 19:10:32 +0000

    misc.php - опечатки, послать => отправить
    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@97 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/misc.php
+++ Russian/misc.php
@@ -7,18 +7,18 @@ $lang_misc = array(
 'Mark forum read redirect'   => 'Все темы в этом форуме были отмечены как прочтённые. Перенаправление…',    //All topics in the specified forum have been marked as read. Redirecting …
 
 // Send e-mail
-'Form e-mail disabled'  	 => 'Пользователь, которому Вы хотите послать сообщение, отказался от получения сообщений по e-mail.',    //The user you are trying to send an e-mail to has disabled form e-mail.
+'Form e-mail disabled'  	 => 'Пользователь, которому Вы хотите отправить сообщение, отказался от получения сообщений по e-mail.',    //The user you are trying to send an e-mail to has disabled form e-mail.
 'Form e-mail errors'		 => '<strong>Внимание!</strong> Исправьте следующие ошибки для отправки вашего сообщения:',    //<strong>Warning!</strong> The following errors must be corrected before your message can be sent:
 'No e-mail subject' 		 => 'Вы должы ввести тему сообщения.',    //You must enter a subject.
 'No e-mail message' 		 => 'Вы должны ввести текст сообщения.',    //You must enter a message.
 'Too long e-mail message'    => 'Длина вашего сообщения %s байт. Это превышает лимит форума в %s байтов.',    //Your message length is %s bytes. This exceeds the %s bytes limit.
 'Email flood'   			 => 'Должно пройти не менее %s секунд между отправками сообщений. Пожалуйста подождите и попробуйте еще раз.',    //At least %s seconds have to pass between sent emails. Please wait a while and try sending again.
-'E-mail sent redirect'  	 => 'Сообщение послано. Перенаправление…',    //E-mail sent. Redirecting …
+'E-mail sent redirect'  	 => 'Сообщение отправлено. Перенаправление…',    //E-mail sent. Redirecting …
 'E-mail subject'			 => 'Заголовок',    //Subject
 'E-mail message'			 => 'Сообщение',    //Message
 'E-mail disclosure note'	 => '<strong>Важно!</strong> Имейте в виду, что при отправлении сообщения через эту форму получатель узнает ваш e-mail адрес.',    //<strong>Important!</strong> When you send an e-mail using this form your e-mail address will be disclosed to the recipient.
 'Write e-mail'  			 => 'Напишите сообщение и нажмите отправить',    //Compose e-mail
-'Send forum e-mail' 		 => 'Послать сообщение пользователю %s используя форум',    //Send e-mail to %s via the forum
+'Send forum e-mail' 		 => 'Отправить сообщение пользователю %s используя форум',    //Send e-mail to %s via the forum
 
 // Report
 'No reason' 				 => 'Вы должны описать что произошло.',    //You must enter a reason.
@@ -46,7 +46,7 @@ $lang_misc = array(
 
 // Hostname lookup
 'Hostname lookup'   		 => 'IP адрес: %1$s<br />Имя хоста: %2$s<br /><br />%3$s',    //The IP address is: %1$s<br />The host name is: %2$s<br /><br />%3$s
-'Show more users'   		 => 'Показать пользователей использовавших этот IP',    //Show more users for this IP
+'Show more users'   		 => 'Показать пользователей, использовавших этот IP',    //Show more users for this IP
 
 // Moderate forum
 'Moderate forum head'   	 => 'Модерирование : %s',    //Moderate : %s
@@ -94,6 +94,6 @@ $lang_misc = array(
 'Confirm post split'		 => 'Подтверждение перемещения сообщений',    //Confirm splitting of all selected posts
 'Delete posts redirect' 	 => 'Сообщения удалены. Перенаправление…',    //Posts deleted. Redirecting …
 'Split posts redirect'  	 => 'Собщения перемещены в новую тему. Перенаправление…',    //Posts split into a new topic. Redirecting …
-'No posts selected' 		 => 'Вы должны выбрать, по крайней мере, одно сообщение, для выполнения этого действия.',    //You must select at least one post.
+'No posts selected' 		 => 'Вы должны выбрать по крайней мере одно сообщение для выполнения этого действия.',    //You must select at least one post.
 
 );
