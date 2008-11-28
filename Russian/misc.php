d3094d952dd6fffeeac0310c2edbd0182798ac3d
Freeman <Freeman@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-11-28 09:01:58 +0000

    "Важно" (о теме), стилистические правки
    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@109 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/misc.php
+++ Russian/misc.php
@@ -3,30 +3,30 @@
 // Language definitions used in various scripts
 $lang_misc = array(
 
-'Mark read redirect'		 => 'Все темы и форумы были отмечены как прочтённые. Перенаправление…',    //All topics and forums have been marked as read. Redirecting …
-'Mark forum read redirect'   => 'Все темы в этом форуме были отмечены как прочтённые. Перенаправление…',    //All topics in the specified forum have been marked as read. Redirecting …
+'Mark read redirect'		 => 'Все темы и разделы отмечены как прочитанные. Перенаправление…',    //All topics and forums have been marked as read. Redirecting …
+'Mark forum read redirect'   => 'Все темы в данном разделе отмечены как прочитанные. Перенаправление…',    //All topics in the specified forum have been marked as read. Redirecting …
 
 // Send e-mail
-'Form e-mail disabled'  	 => 'Пользователь, которому Вы хотите отправить сообщение, отказался от получения сообщений по e-mail.',    //The user you are trying to send an e-mail to has disabled form e-mail.
-'Form e-mail errors'		 => '<strong>Внимание!</strong> Исправьте следующие ошибки для отправки вашего сообщения:',    //<strong>Warning!</strong> The following errors must be corrected before your message can be sent:
-'No e-mail subject' 		 => 'Вы должы ввести тему сообщения.',    //You must enter a subject.
-'No e-mail message' 		 => 'Вы должны ввести текст сообщения.',    //You must enter a message.
-'Too long e-mail message'    => 'Длина вашего сообщения %s байт. Это превышает лимит форума в %s байтов.',    //Your message length is %s bytes. This exceeds the %s bytes limit.
-'Email flood'   			 => 'Должно пройти не менее %s секунд между отправками сообщений. Пожалуйста подождите и попробуйте еще раз.',    //At least %s seconds have to pass between sent emails. Please wait a while and try sending again.
-'E-mail sent redirect'  	 => 'Сообщение отправлено. Перенаправление…',    //E-mail sent. Redirecting …
-'E-mail subject'			 => 'Заголовок',    //Subject
+'Form e-mail disabled'  	 => 'Пользователь, которому вы хотите отправить письмо, запретил отправку ему сообщений e-mail.',    //The user you are trying to send an e-mail to has disabled form e-mail.
+'Form e-mail errors'		 => '<strong>Внимание!</strong> Исправьте следующие ошибки для отправки вашего письма:',    //<strong>Warning!</strong> The following errors must be corrected before your message can be sent:
+'No e-mail subject' 		 => 'Необходимо ввести тему письма.',    //You must enter a subject.
+'No e-mail message' 		 => 'Необходимо ввести текст письма.',    //You must enter a message.
+'Too long e-mail message'    => 'Длина вашего сообщения - %s байт. Это превышает лимит форума в %s байтов.',    //Your message length is %s bytes. This exceeds the %s bytes limit.
+'Email flood'   			 => 'Должно пройти не менее %s секунд между отправками писем. Пожалуйста, подождите немного и попробуйте еще раз.',    //At least %s seconds have to pass between sent emails. Please wait a while and try sending again.
+'E-mail sent redirect'  	 => 'Письмо отправлено. Перенаправление…',    //E-mail sent. Redirecting …
+'E-mail subject'			 => 'Тема',    //Subject
 'E-mail message'			 => 'Сообщение',    //Message
-'E-mail disclosure note'	 => '<strong>Важно!</strong> Имейте в виду, что при отправлении сообщения через эту форму получатель узнает ваш e-mail адрес.',    //<strong>Important!</strong> When you send an e-mail using this form your e-mail address will be disclosed to the recipient.
-'Write e-mail'  			 => 'Напишите сообщение и нажмите отправить',    //Compose e-mail
-'Send forum e-mail' 		 => 'Отправить сообщение пользователю %s используя форум',    //Send e-mail to %s via the forum
+'E-mail disclosure note'	 => '<strong>Важно!</strong> При отправке письма через эту форму ваш адрес e-mail будет виден получателю.',    //<strong>Important!</strong> When you send an e-mail using this form your e-mail address will be disclosed to the recipient.
+'Write e-mail'  			 => 'Составьте письмо',    //Compose e-mail
+'Send forum e-mail' 		 => 'Отправить письмо %s, используя форум',    //Send e-mail to %s via the forum
 
 // Report
-'No reason' 				 => 'Вы должны описать что произошло.',    //You must enter a reason.
-'Report flood'  			 => 'Должно пройти не менее %s секунд между сообщениями модераторам. Пожалуйста подождите и попробуйте еще раз.',    //At least %s seconds have to pass between reports. Please wait a while and try sending again.
-'Report redirect'   		 => 'Сообщение отправлено. Перенаправление…',    //Post reported. Redirecting …
+'No reason' 				 => 'Необходимо ввести причину обращения.',    //You must enter a reason.
+'Report flood'  			 => 'Должно пройти не менее %s секунд между обращениями к модератору. Пожалуйста, подождите немного и попробуйте еще раз.',    //At least %s seconds have to pass between reports. Please wait a while and try sending again.
+'Report redirect'   		 => 'Обращение отправлено. Перенаправление…',    //Post reported. Redirecting …
 'Report post'   			 => 'Сообщить модератору',    //Report post
-'Reason'					 => 'Причина сообщения',    //Reason
-'Reason help'   			 => 'Укажите кратко причину сообщения',    //Enter a short reason why you are reporting this post.
+'Reason'					 => 'Причина обращения',    //Reason
+'Reason help'   			 => 'Укажите краткую причину обращения',    //Enter a short reason why you are reporting this post.
 
 // Subscriptions
 'Already subscribed'		 => 'Вы уже подписаны на эту тему.',    //You are already subscribed to this topic.
@@ -37,63 +37,63 @@ $lang_misc = array(
 // General forum and topic moderation
 'Moderate forum'			 => 'Модерировать форум',    //Moderate forum
 'Select'					 => 'Выбрать',    //Select
-'Move'  					 => 'Переместить',    //Move
-'Merge' 					 => 'Соединить',    //Merge
+'Move'  					 => 'Перенести',    //Move
+'Merge' 					 => 'Объединить',    //Merge
 'Open'  					 => 'Открыть',    //Open
 'Close' 					 => 'Закрыть',    //Close
-'Select all'				 => 'Выбрать все',    //Select all
+'Select all'				 => 'Выбрать всё',    //Select all
 
 
 // Hostname lookup
-'Hostname lookup'   		 => 'IP адрес: %1$s<br />Имя хоста: %2$s<br /><br />%3$s',    //The IP address is: %1$s<br />The host name is: %2$s<br /><br />%3$s
-'Show more users'   		 => 'Показать пользователей, использовавших этот IP',    //Show more users for this IP
+'Hostname lookup'   		 => 'IP-адрес: %1$s<br />Имя узла: %2$s<br /><br />%3$s',    //The IP address is: %1$s<br />The host name is: %2$s<br /><br />%3$s
+'Show more users'   		 => 'Показать пользователей с этим IP',    //Show more users for this IP
 
 // Moderate forum
 'Moderate forum head'   	 => 'Модерирование : %s',    //Moderate : %s
 'Topics'					 => 'Темы',    //Topics
-'Move topic'				 => 'Переместить тему',    //Move topic
-'Move topics'   			 => 'Переместить темы',    //Move topics
-'Merge topics'  			 => 'Соединить темы',    //Merge topics
+'Move topic'				 => 'Перенести тему',    //Move topic
+'Move topics'   			 => 'Перенести темы',    //Move topics
+'Merge topics'  			 => 'Объединить темы',    //Merge topics
 'Delete topics' 			 => 'Удалить темы',    //Delete topics
 'Delete topic'  			 => 'Удалить тему',    //Delete topic
-'To new forum'  			 => 'в новый форум',    //to a new forum
+'To new forum'  			 => 'в новый раздел',    //to a new forum
 'Move to'   				 => 'Переместить в',    //Destination forum
-'Redirect topic'			 => 'Перенаправление',    //Redirect setting
-'Nowhere to move'   		 => 'Нет ни одного форума, в который вы могли бы переносить темы.',    //There are no forums into which you can move topics.
+'Redirect topic'			 => 'Перенаправление старой темы',    //Redirect setting
+'Nowhere to move'   		 => 'Нет разделов, в который можно перенести темы.',    //There are no forums into which you can move topics.
 'Leave redirect'			 => 'Оставить ссылку на темы/тему в этом форуме',    //Leave a redirect topic in the forum from which the topic was moved.
-'Leave redirects'   		 => 'Оставить ссылку на перемещаемые темы/тему в этом форуме.',    //Leave redirect topics in the forum from which topics were moved.
-'Leave merge redirects' 	 => 'Оставить ссылку на соединяемые темы/тему.',    //Leave redirect topics for those topics which were merged.
-'Move topic redirect'   	 => 'Тема перемещена. Перенаправление…',    //Topic moved. Redirecting …
-'Move topics redirect'  	 => 'Темы перемещены. Перенаправление…',    //Topics moved. Redirecting …
+'Leave redirects'   		 => 'Оставить ссылку в разделе, где тема находилась ранее.',    //Leave redirect topics in the forum from which topics were moved.
+'Leave merge redirects' 	 => 'Оставить ссылки на объединяемые темы.',    //Leave redirect topics for those topics which were merged.
+'Move topic redirect'   	 => 'Тема перенесена. Перенаправление…',    //Topic moved. Redirecting …
+'Move topics redirect'  	 => 'Темы перенесены. Перенаправление…',    //Topics moved. Redirecting …
 'Merge topics redirect' 	 => 'Темы соединены. Перенаправление…',    //Topics merged. Redirecting …
-'Delete topic comply'   	 => 'Вы уверены, что хотите удалить выделенную тему?',    //Are you sure you want to delete the selected topic?
-'Delete topics comply'  	 => 'Вы уверены, что хотите удалить выделенные темы?',    //Are you sure you want to delete all the selected topics?
+'Delete topic comply'   	 => 'Вы уверены, что хотите удалить выбранную тему?',    //Are you sure you want to delete the selected topic?
+'Delete topics comply'  	 => 'Вы уверены, что хотите удалить выбранные темы?',    //Are you sure you want to delete all the selected topics?
 'Delete topic redirect' 	 => 'Тема удалена. Перенаправление…',    //Topic deleted. Redirecting …
 'Delete topics redirect'	 => 'Темы удалены. Перенаправление…',    //Topics deleted. Redirecting …
 'Open topic redirect'   	 => 'Тема открыта. Перенаправление…',    //Topic opened. Redirecting …
 'Open topics redirect'  	 => 'Темы открыты. Перенаправление…',    //Topics opened. Redirecting …
 'Close topic redirect'  	 => 'Тема закрыта. Перенаправление…',    //Topic closed. Redirecting …
 'Close topics redirect' 	 => 'Темы закрыты. Перенаправление…',    //Topics closed. Redirecting …
-'No topics selected'		 => 'Вы должны выбрать, по крайней мере, одну тему, для выполнения этого действия.',    //You must select at least one topic.
-'Stick topic redirect'  	 => 'Тема выделена. Перенаправление…',    //Topic is now sticky. Redirecting …
-'Unstick topic redirect'	 => 'Выделение с темы снято. Перенаправление…',    //Topic no longer sticky. Redirecting …
-'Merge error'   			 => 'Вы должны выбрать больше одной темы для соединения.',    //You should select more than 1 topic to merge.
+'No topics selected'		 => 'Нужно выбрать хотя бы одну тему.',    //You must select at least one topic.
+'Stick topic redirect'  	 => 'Тема отмечена как важная. Перенаправление…',    //Topic is now sticky. Redirecting …
+'Unstick topic redirect'	 => 'Отметка важности с темы снята. Перенаправление…',    //Topic no longer sticky. Redirecting …
+'Merge error'   			 => 'Для объединения нужно выбрать более 1-й темы.',    //You should select more than 1 topic to merge.
 
 // Moderate topic
 'Posts' 					 => 'Сообщения',    //Posts
-'Delete posts'  			 => 'Удалить сообщения',    //Delete selected posts
-'Split posts'   			 => 'Переместить выделенные сообщения',    //Split selected posts
+'Delete posts'  			 => 'Удалить выбранные сообщения',    //Delete selected posts
+'Split posts'   			 => 'Разделить выбранные сообщения',    //Split selected posts
 'Delete whole topic'		 => 'Удалить всю тему',    //Delete whole topic
 'Moderate topic head'   	 => 'Модерирование темы : %s',    //Moderate topic : %s
 'New subject'   			 => 'Новый заголовок темы:',    //New topic subject:
-'Select post'   			 => 'Выделить сообщение',    //Select post
-'Confirm post delete'   	 => 'Подтвердите удаление всех выбранных сообщений',    //Confirm deletion of all selected posts
-'Confirm topic delete'  	 => 'Подтвердите удаление всех выбранных тем',    //Confirm deletion of all selected topics
-'Confirm topic split'   	 => 'Подвердите перемещение всех выделенных сообщений в новую тему',    //Confirm splitting of all selected posts into a new topic
-'Confirm topic merge'   	 => 'Подтверждение соединения тем в одну',    //Confirm merging of all selected topics into one topic
-'Confirm post split'		 => 'Подтверждение перемещения сообщений',    //Confirm splitting of all selected posts
+'Select post'   			 => 'Выбрать сообщение',    //Select post
+'Confirm post delete'   	 => 'Подтверждение удаления выбранных сообщений',    //Confirm deletion of all selected posts
+'Confirm topic delete'  	 => 'Подтверждение удаления выбранных тем',    //Confirm deletion of all selected topics
+'Confirm topic split'   	 => 'Подтверждение переноса выбранных сообщений в новую тему',    //Confirm splitting of all selected posts into a new topic
+'Confirm topic merge'   	 => 'Подтверждение объединения выбранных тем в одну',    //Confirm merging of all selected topics into one topic
+'Confirm post split'		 => 'Подтверждение разделения сообщений',    //Confirm splitting of all selected posts
 'Delete posts redirect' 	 => 'Сообщения удалены. Перенаправление…',    //Posts deleted. Redirecting …
-'Split posts redirect'  	 => 'Собщения перемещены в новую тему. Перенаправление…',    //Posts split into a new topic. Redirecting …
-'No posts selected' 		 => 'Вы должны выбрать по крайней мере одно сообщение для выполнения этого действия.',    //You must select at least one post.
+'Split posts redirect'  	 => 'Сообщения выделены в новую тему. Перенаправление…',    //Posts split into a new topic. Redirecting …
+'No posts selected' 		 => 'Нужно выбрать хотя бы одно сообщение.',    //You must select at least one post.
 
 );
