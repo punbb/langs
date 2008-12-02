45b5f19651d238e462a5a91a6dd90d903a78f8e4
TuxPirate <TuxPirate@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-12-02 16:57:37 +0000

    admin_reports.php - сигнал = > обращение
    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@117 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/admin_reports.php
+++ Russian/admin_reports.php
@@ -3,20 +3,20 @@
 // Language definitions used in all admin files
 $lang_admin_reports = array(
 
-'No reports selected'			=>	'Не выбраны сигналы, которые отмечены как прочтённые.',
-'Reports marked read'			=>	'Сигналы отмечены как обработанные.',
-'New reports heading'			=>	'Новые сигналы (выберите и отметьте как прочтённый один или несколько)',
-'Empty reports heading'			=>	'Сигналы не поступали',
-'Reported by'					=>	'Поступил от %s',
+'No reports selected'			=>	'Не выбраны обращения, которые отмечены, как прочтённые.',
+'Reports marked read'			=>	'Обращения отмечены, как обработанные.',
+'New reports heading'			=>	'Новые обращения (выберите и отметьте, как прочтённый, один или несколько)',
+'Empty reports heading'			=>	'Обращения не поступали',
+'Reported by'					=>	'Поступило от %s',
 'Deleted forum'					=>	'Удалён форум',
 'Deleted topic'					=>	'Удалена тема',
 'Deleted post'					=>	'Удалено сообщение',
 'Deleted user'					=>	'Удалён пользователь',
 'Mark read'						=>	'Обработано',
-'Select report'					=>	'Выбрать сигнал',
-'No new reports'				=>	'(нет сигналов которые не обработаны)',
-'Read reports heading'			=>	'Последние 10 обработанных сигналов',
-'No reports'					=>	'Нет обработанных либо не обработанных сигналов для отображения.',
+'Select report'					=>	'Выбрать обращение',
+'No new reports'				=>	'(нет обращений, которые не обработаны)',
+'Read reports heading'			=>	'Последние 10 обработанных обращений',
+'No reports'					=>	'Нет обработанных либо не обработанных обращений для отображения.',
 'Marked read by'				=>	'[ Обработано %s %s ]',
 
 );
