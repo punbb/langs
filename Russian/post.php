9229670f5a6f50fe5d8dcbb804377df5b2f02d0c
putnik <putnik@137235ca-13ef-7742-bd68-8e27fb1756a1>
2008-12-22 02:23:06 +0000

    мелочи
    git-svn-id: https://mysvn.ru/punbb/punbb/trunk@129 137235ca-13ef-7742-bd68-8e27fb1756a1

--- Russian/post.php
+++ Russian/post.php
@@ -11,13 +11,13 @@ $lang_post = array(
 'Too long message'  	 => 'Длина вашего сообщения %s байт. Это превышает лимит форума в %s байтов.',    //Your post length is %s bytes. This exceeds the %s bytes limit.
 'All caps message'  	 => 'Сообщение не может содержать только заглавные буквы.',    //Posts cannot contain only capital letters.
 'CSRF token mismatch'    => 'Обнаружена проблема безопасности. Скорее всего, прошло много времени с тех пор как вы открыли страницу. Нажмите кнопку «Отправить» еще раз.',    //Unable to confirm security token. A likely cause for this is that some time passed between when you first entered the page and when you posted the form. Clicking "Submit" again should solve the problem.
-'Invalid e-mail'		 => 'Неправильный E-mail адрес.',    //The e-mail address you entered is invalid.
+'Invalid e-mail'		 => 'Неправильный адрес e-mail.',    //The e-mail address you entered is invalid.
 
 // Posting
 'Compose your reply'	 => 'Напишите и отправьте новый ответ',    //Compose and post your new reply
 'Compose your topic'	 => 'Создайте новую тему',    //Compose and post your new topic
 'Compose edited reply'   => 'Отредактируйте и отправьте ваш ответ',    //Compose and post your edited reply
-'Compose edited topic'   => 'Отредактируйте и отправьте ваше тему',    //Compose and post your edited topic
+'Compose edited topic'   => 'Отредактируйте и отправьте вашу тему',    //Compose and post your edited topic
 'Guest post legend' 	 => 'Важная информация для гостей',    //Required information for guests
 'Post errors'   		 => '<strong>Внимание!</strong> Исправьте следующие ошибки, чтобы добавить сообщение:',    //<strong>Warning!</strong> The following errors must be corrected before your message can be posted:
 'Guest name'			 => 'Ваше имя',    //Guest name
